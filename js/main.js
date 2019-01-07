531d95b833128038130bb785d2cbd149b106c9b4
Muradov Cavanshir Imanqulu  <Mura_lw91@itstep.edu.az>
2019-01-07 11:44:53 +0400

    js ilk is

---
+++ js/main.js
@@ -0,0 +1,125 @@
+
+function times(){
+    document.getElementById("folse").style.backgroundColor="red";
+    document.getElementById("icon-folse").style.display="block";
+}
+
+function times1(){
+    document.getElementById("folse1").style.backgroundColor="red";
+    document.getElementById("icon-folse1").style.display="block";
+}
+
+function times2(){
+    document.getElementById("folse2").style.backgroundColor="red";
+    document.getElementById("icon-folse2").style.display="block";
+}
+
+function check(){
+    document.getElementById("true").style.backgroundColor="green";
+    document.getElementById("icon-true").style.display="block";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function calc(num1,num2){
+//   let result = num1;
+//  for(let i=1; i<num2; i++){
+//   result = result * num1
+  
+//  }
+
+//  return result
+// }
+
+// alert(calc(6,3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function GivenMoney() {
+//     let money=prompt(`pulu daxil edin`,0)
+
+//     let pricing=1;
+
+//     alert(money-pricing)
+
+//   }
+
+//   GivenMoney()
+
+//   GivenMoney()
+
+//   GivenMoney()
+
+
+  
+
+
+
+
+
+
+
+// function name(){
+
+//     let name=`cavansir`;
+
+//     let parol="123";
+
+//     let pro=prompt(`adi daxil edin`,``);
+
+//     if(pro==name){
+
+//         let prof=prompt(`parol daxil edin`,``);
+
+
+//         if(prof==parol){
+//             alert(`xos geldiniz`);
+//         }
+
+//         else if(prof==""){
+//             alert(`chalange`);
+//         }
+    
+//         else{
+//             alert(`parol sehvdir`);
+//         }
+    
+//     }
+
+//     else if(pro==""){
+//         alert(`cahalenge`);
+//     }
+
+//     else{
+//         alert(`ad sehvdir`);
+//     }
+// }
+// name();
+
+
+
